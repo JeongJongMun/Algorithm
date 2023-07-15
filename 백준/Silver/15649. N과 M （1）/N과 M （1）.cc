c4331890376b5e6bf8e1140b0d823663181e6233
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2023-07-16 00:46:08 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int answer[8];
+bool isVisited[8];
+
+void DFS(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << answer[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (!isVisited[i]) {
+			isVisited[i] = true;
+			answer[depth] = i;
+			DFS(depth + 1);
+			isVisited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	DFS(0);
+
+	return 0;
+}
