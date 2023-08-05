8592dffadc536d5d0aeb699bc6374cc302b05784
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2023-08-06 01:25:24 +0900

    [Silver IV] Title: blobyum, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/24499. blobyum/blobyum.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 애플 파이의 개수, 먹으려는 애플 파이 개수
+int N, K;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> K;
+	
+	vector<int> apple_pie(N + 1);
+
+	int sum = 0, x;
+	for (int i = 1; i < N + 1; i++) {
+		cin >> x;
+		sum += x;
+		apple_pie[i] = sum;
+	}
+
+	int start = 0;
+	int end = K;
+	int answer = 0;
+	for (; end < N + 1; end++, start++) {
+		//cout << end << " " << start << " " << apple_pie[end] - apple_pie[start] << endl;
+		answer = max(answer, apple_pie[end] - apple_pie[start]);
+	}
+	//cout << endl;
+	end = 1;
+	for (; start < N; end++, start++) {
+		//cout << end << " " << start << " " << apple_pie[N] - apple_pie[start] + apple_pie[end] << endl;
+		answer = max(answer, apple_pie[N] - apple_pie[start] + apple_pie[end]);
+	}
+
+	cout << answer;
+
+	return 0;
+}
+/*
+7 4
+6 7 1 2 3 4 5
+*/
