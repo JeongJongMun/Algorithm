e058e9f697f4f60056baf9bdaa0b7aeafedb1c1a
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2023-09-11 14:42:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+int N;
+long long arr[1001];
+
+int main() {
+
+	cin >> N;
+
+	arr[1] = 1;
+	arr[2] = 2;
+
+	for (int i = 3; i <= N; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+	}
+	
+	cout << arr[N];
+
+	return 0;
+}
