8c3e5f0333bb4a775ffc0b4c4006fa943a260747
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2023-11-16 23:14:24 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		int a, b; cin >> a >> b;
+		cout << a + b << '\n';
+	}
+
+
+	return 0;
+}
