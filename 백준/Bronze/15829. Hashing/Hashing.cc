ec94a5ac256ada56e46999b749b060f749945124
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2023-12-20 18:10:25 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define ULL unsigned long long
 using namespace std;
 
 int main() {
@@ -7,13 +8,21 @@ int main() {
 	int n; cin >> n;
 	string str; cin >> str;
 
-	unsigned long long ans = 0;
-
+	ULL ans = 0;
+	ULL modular = 1234567891;
+	ULL p = 1;
 	for (int i = 0; i < n; i++) {
-		ans += (str[i] - 'a' + 1) * pow(31, i);
+		if (i != 0) p *= 31;
+		p %= modular;
+		ans += (str[i] - 'a' + 1) * p;
+		ans %= modular;
 	}
 
-	cout << ans;
+	cout << ans % modular;
 
 	return 0;
 }
+/*
+50
+abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde
+*/
