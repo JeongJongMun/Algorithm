c50d5cf5f6c0e577cfcfe26fcf91335c39518e96
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-04 01:48:44 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	string a;
+
+	while (true) {
+		if (cin.eof()) break;
+		getline(cin, a);
+		cout << a << '\n';
+	}
+
+	return 0;
+}
