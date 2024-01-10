527298aebd3e066ace05b91db7e5d1c2a578c59f
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-11 02:04:55 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	long long N, M; cin >> N >> M;
+	cout << abs(N - M);
+	
+	return 0;
+}
