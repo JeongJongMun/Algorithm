531e3e642b21d686058ed7d27965cb3cdbf1b080
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-16 19:53:25 +0900

    [Silver V] Title: 종이자르기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,22 +1,13 @@
 n, m = map(int, input().split())
-
-width = [0, n]
-length = [0, m]
-
-k = int(input())
-for i in range(k):
-    wl, num = map(int, input().split())
-    width.append(num) if wl else length.append(num)
-    
-width.sort()
-length.sort()
-
+x = [0, n]
+y = [0, m]
+for i in range(int(input())):
+    xy, num = map(int, input().split())
+    x.append(num) if xy else y.append(num)
+x.sort()
+y.sort()
 w_max = 0
 l_max = 0
-for i in range(len(width) - 1):
-    w_max = max(w_max, width[i + 1] - width[i])
-
-for i in range(len(length) - 1):
-    l_max = max(l_max, length[i + 1] - length[i])
-    
+w_max = max([max(w_max, x[i + 1] - x[i]) for i in range(len(x) - 1)])
+l_max = max([max(l_max, y[i + 1] - y[i]) for i in range(len(y) - 1)])
 print(w_max * l_max)
