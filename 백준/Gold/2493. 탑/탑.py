265d3462ff6e0b1afbbeb17941ea3f45b13c5137
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-17 17:38:46 +0900

    [Gold V] Title: 탑, Time: 524 ms, Memory: 98280 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -5,14 +5,11 @@ stack = []
 
 for i in range(n):
     while stack:
-        # 스택의 맨 위 타워의 높이가 현재 타워의 높이보다 크다면 수신 가능 
-        if stack[-1][1] > tower[i]:
-            ans[i] = stack[-1][0] + 1
+        if tower[stack[-1]] > tower[i]:
+            ans[i] = stack[-1] + 1
             break
-        # 아니라면 수신 불가 -> 삭제
         else:
             stack.pop()
-    # 각 타워의 인덱스와 높이를 저장
-    stack.append([i, tower[i]])
+    stack.append(i)
  
 print(*ans)
