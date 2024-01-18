b030085ce4aafc2fd0d9a08b5d252407dc43c9a0
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-18 11:19:35 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,19 +1,9 @@
 input = int(input())
-cnt = 0
-n = input
+cnt, n = 0, input
 while 1:
     cnt += 1
-
-    if n < 10:
-        nn = n + n*10
-    else:
-        nn = n % 10 + n // 10
+    if n < 10: nn = n + n*10
+    else: nn = n % 10 + n // 10
     n = (n % 10)*10 + nn % 10
-
-    if n == input:
-        break
- 
+    if n == input: break
 print(cnt)
-'''
-1 -> 0 + 1 -> 11 ->
-'''
