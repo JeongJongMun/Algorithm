9eb9619d479cedef50cec9a7074d6c793ee67fd1
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-18 11:02:34 +0900

    [Silver I] Title: 쿼드트리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,25 +1,23 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
 arr = []
 for _ in range(n):
-    arr.append(list(input()))
+    arr.append(list(input().strip()))
     
-# 시작 x, 시작 y, n만큼 검사
 def quad_tree(x, y, n):
     check = arr[x][y]
-    flag = False
     for i in range(n):
         for j in range(n):
-            if arr[x+j][y+i] != check: flag = True
-    
-    if flag:
-        nn = n//2
-        print('(', end='')
-        quad_tree(x, y, nn)
-        quad_tree(x, y + nn, nn)
-        quad_tree(x + nn, y, nn)
-        quad_tree(x + nn, y + nn, nn)
-        print(')', end='')
-    else:
-        print(check, end='')
-        
+            if arr[x+j][y+i] != check:
+                nn = n//2
+                print('(', end='')
+                quad_tree(x, y, nn)
+                quad_tree(x, y + nn, nn)
+                quad_tree(x + nn, y, nn)
+                quad_tree(x + nn, y + nn, nn)
+                print(')', end='')
+                return
+    print(check, end='')
+
 quad_tree(0, 0, n)
