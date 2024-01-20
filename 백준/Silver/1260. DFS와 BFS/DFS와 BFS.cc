d5821b27f7fc04685fa963df9996d3c5e972271f
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-20 11:24:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,58 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, V;
-bool visited[1001];
-vector<int> graph[1001];
+vector<int> DFS(vector<vector<int>>& graph, int s) {
+	vector<bool> visited(graph.size(), false);
+	vector<int> answer;
+	stack<int> stack({ s });
 
-void dfs(int node) {
-	visited[node] = true;
-	cout << node << ' ';
-	for (int i = 0; i < graph[node].size(); i++) {
-		int next = graph[node][i];
-		if (!visited[next]) dfs(next);
-	}
-}
-
-void bfs(int node) {
-	queue<int> q;
-	q.push(node);
-	visited[node] = true;
-
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		cout << cur << ' ';
-
-		for (int i = 0; i < graph[cur].size(); i++) {
-			int next = graph[cur][i];
-			if (!visited[next]) {
-				q.push(next);
-				visited[next] = true;
+	while (!stack.empty()) {
+		int node = stack.top();
+		stack.pop();
+		if (!visited[node]) {
+			visited[node] = true;
+			answer.emplace_back(node);
+			for (int i = 0; i < graph[node].size(); i++) {
+				stack.push(graph[node][i]);
 			}
 		}
 	}
+	return answer;
+}
+
+
+vector<int> BFS(vector<vector<int>>& graph, int s) {
+	vector<bool> visited(graph.size(), false);
+	vector<int> answer;
+	queue<int> queue({ s });
+
+	while (!queue.empty()) {
+		int node = queue.front();
+		queue.pop();
+		if (!visited[node]) {
+			visited[node] = true;
+			answer.emplace_back(node);
+			for (int i = 0; i < graph[node].size(); i++) {
+				queue.push(graph[node][i]);
+			}
+		}
+	}
+	return answer;
 }
 
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> N >> M >> V;
-	
-	int a, b;
-	for (int i = 0; i < M; i++) {
-		 cin >> a >> b;
-		 graph[a].push_back(b);
-		 graph[b].push_back(a);
-	}
-	for (int i = 0; i <= N; i++) {
-		sort(graph[i].begin(), graph[i].end());
+	int v, e, s; cin >> v >> e >> s;
+	vector<vector<int>> graph(v + 1);
+	for (int i = 0; i < e; i++) {
+		int a, b; cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
 	}
 
-	dfs(V);
-	memset(visited, false, sizeof(visited));
-	cout << '\n';
-	bfs(V);
+	for (int i = 1; i <= v; i++) sort(graph[i].rbegin(), graph[i].rend());
+	vector<int> answer = DFS(graph, s);
+	for (auto & i : answer) cout << i << " ";
+	cout << "\n";
+
+	for (int i = 1; i <= v; i++) sort(graph[i].begin(), graph[i].end());
+	vector<int> answer2 = BFS(graph, s);
+	for (auto & i : answer2) cout << i << " ";
+	cout << "\n";
 
 	return 0;
 }
