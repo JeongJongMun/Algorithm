0d1240a8f32fead0561fe67a128127d2a9cef500
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-22 17:53:56 +0900

    [Gold III] Title: 아침 산책, Time: 732 ms, Memory: 69472 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,7 +1,20 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+INF = 1e9
 
+# 1) 1 - ... - 1 -> 시작과 끝은 무조건 1
+# 2) 1 - 0 - ... - 0 - 1 -> 시작과 끝 사이는 무조건 0
+# 3) 두 점 사이의 경로는 유일
+# 4) N개의 노드와 N-1개의 에지
+# n^2 불가능
+# n log n 가능
+# n+e log n 가능
+
+
+# @ a노드에서 시작해서 모든 노드로 가는 산책 경로 구하기
+# 1. 0인 경우 방문 처리 하고 큐에 인접 노드 넣기 (지나가기)
+# 2. 1인 경우 1인 노드를 방문 처리하고 cnt++ (큐에 집어 넣을 필요도 없음 어짜피 못지나감)
 def walk(graph, inside, visited, start):
     _visited = visited.copy()
     que = deque([start])
