76aff832a880940e22770a2ea2fc5e8b0e1cfe47
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-22 20:24:05 +0900

    [Gold III] Title: 아침 산책, Time: 508 ms, Memory: 71128 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,44 +1,34 @@
-import sys
-from collections import deque
+import sys, math
 input = sys.stdin.readline
-INF = 1e9
 
-# 1) 1 - ... - 1 -> 시작과 끝은 무조건 1
-# 2) 1 - 0 - ... - 0 - 1 -> 시작과 끝 사이는 무조건 0
-# 3) 두 점 사이의 경로는 유일
-# 4) N개의 노드와 N-1개의 에지
-# n^2 불가능
-# n log n 가능
-# n+e log n 가능
-
-
-# @ a노드에서 시작해서 모든 노드로 가는 산책 경로 구하기
-# 1. 0인 경우 방문 처리 하고 큐에 인접 노드 넣기 (지나가기)
-# 2. 1인 경우 1인 노드를 방문 처리하고 cnt++ (큐에 집어 넣을 필요도 없음 어짜피 못지나감)
 def walk(graph, inside, visited, start):
-    _visited = visited.copy()
-    que = deque([start])
+    stack = [start]
+    visited[start] = 1
     cnt = 0
-    while que:
-        node = que.popleft()
+    while stack:
+        node = stack.pop()
         for next_node in graph[node]:
-            if not _visited[next_node] and not inside[next_node]:
-                que.append(next_node)
-                _visited[next_node] = 1
-            elif inside[next_node] and next_node != start:
+            if inside[next_node]:
                 cnt += 1
-    return cnt
+                visited[next_node] = 1
+            if not visited[next_node]:
+                stack.append(next_node)
+                visited[next_node] = 1
+    return math.factorial(cnt)/math.factorial(cnt-2) if cnt >= 2 else 0
                   
 def main():
     v = int(input().strip())
     inside = list(map(int, input().strip()))
     inside = [0] + inside
     graph = [[] for _ in range(v + 1)]
-    visited = [1 if inside[i] else 0 for i in range(v+1)]
+    visited = [0 for _ in range(v + 1)]
+    ans = 0
+
     for _ in range(v - 1):
         a, b = map(int, input().strip().split())
         graph[a].append(b)
         graph[b].append(a)
+        ans += 2 if inside[a] and inside[b] else 0
 
     flag = True
     for i in range(2, len(graph) -1):
@@ -48,13 +38,12 @@ def main():
     if flag:
         print(max(0, (sum(inside) - 1) * 2))
         exit()
-
-    ans = 0
+        
     for start in range(1, v + 1):
-        if visited[start]:
+        if not inside[start] and not visited[start]:
             ans += walk(graph, inside, visited, start)
     
-    print(ans)
+    print(int(ans))
     
 if __name__ == '__main__':
     main()
