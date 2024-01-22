89d44a7d4b0232e2ff35db34369505af79c56f86
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-22 12:56:46 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2232 ms, Memory: 103016 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -3,32 +3,37 @@ from collections import deque
 input = sys.stdin.readline
 INF = 1e9
     
-def bfs(graph, visited, dist, start):
+def bfs(graph, k, start):
+    visited = [False] * (len(graph) + 1)
+    dist = [INF] * (len(graph) + 1)
+    dist[start] = 0
     que = deque([start])
+    
     while que:
-        node= que.popleft()
+        node = que.popleft()
         visited[node] = True
         for next_node in graph[node]:
             if not visited[next_node]:
                 que.append(next_node)
                 dist[next_node] = min(dist[next_node], dist[node] + 1)
+    
+    return [idx for idx, cost in enumerate(dist) if cost == k] or [-1]
                     
 def main():
     v, e, k, x = map(int, input().strip().split())
     graph = [[] for _  in range(v + 1)]
-    visited = [False for _ in range(v + 1)]
-    dist = [INF for _ in range(v + 1)]
-    dist[x] = 0
     for _ in range(e):
         a, b = map(int, input().strip().split())
         graph[a].append(b)
-    bfs(graph, visited, dist, x)    
-    ans = [(idx, cost) for idx, cost in enumerate(dist) if cost == k]
+    ans = bfs(graph, k, x)
+    for idx in ans: print(idx)
+        
+    # ans = [(idx, cost) for idx, cost in enumerate(dist) if cost == k]
 
-    if ans:
-        for idx, _ in ans:
-            print(idx)
-    else: print(-1)
+    # if ans:
+    #     for idx, _ in ans:
+    #         print(idx)
+    # else: print(-1)
             
 if __name__ == '__main__':
     main()
