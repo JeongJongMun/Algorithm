bb52a14ddb98efb4db12294aed91810c7ff17ade
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-22 16:38:30 +0900

    [Gold III] Title: 아침 산책, Time: 536 ms, Memory: 147688 KB, Score: 73 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -15,36 +15,45 @@ INF = 1e9
 # @ a노드에서 시작해서 모든 노드로 가는 산책 경로 구하기
 # 1. 0인 경우 방문 처리 하고 큐에 인접 노드 넣기 (지나가기)
 # 2. 1인 경우 1인 노드를 방문 처리하고 cnt++ (큐에 집어 넣을 필요도 없음 어짜피 못지나감)
-def walk(graph, inout, visited, check, start):
+def walk(graph, inside, visited, start):
     _visited = visited.copy()
     que = deque([start])
     cnt = 0
     while que:
         node = que.popleft()
         for next_node in graph[node]:
-            if not _visited[next_node] and inout[next_node] == 0:
+            if not _visited[next_node] and not inside[next_node]:
                 que.append(next_node)
-                _visited[next_node] = True
-            elif inout[next_node] == 1 and next_node != start:
+                _visited[next_node] = 1
+            elif inside[next_node] and next_node != start:
                 cnt += 1
     return cnt
                   
 def main():
     v = int(input().strip())
-    inout = list(map(int, input().strip()))
-    inout = [0] + inout
+    inside = list(map(int, input().strip()))
+    inside = [0] + inside
+    
+    
     graph = [[] for _ in range(v + 1)]
-    visited = [True if inout[i] else False for i in range(v+1) ]
-    # check = [[0] * (v + 1)] * (v + 1)
+    visited = [1 if inside[i] else 0 for i in range(v+1) ]
     for _ in range(v - 1):
         a, b = map(int, input().strip().split())
         graph[a].append(b)
         graph[b].append(a)
-    
+    flag = True
+    for i in range(2, len(graph) -1):
+        if len(graph[i]) != 2:
+            flag = False
+            break
+    if flag:
+        print((sum(inside) - 1) * 2)
+        exit()
+
     ans = 0
     for start in range(1, v + 1):
         if visited[start]:
-            ans += walk(graph, inout, visited, _, start)
+            ans += walk(graph, inside, visited, start)
     
     print(ans)
     
