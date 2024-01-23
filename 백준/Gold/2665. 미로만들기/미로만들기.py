d89fc5a9d346d77548ab63b17f09dbac4f6fcb79
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-23 23:39:57 +0900

    [Gold IV] Title: 미로만들기, Time: 76 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,20 +1,18 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-INF = 1e9
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+dir = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 
 def BFS(room):
     N = len(room)
-    cnt = [[5000] * N for _ in range(N)]
+    cnt = [[2500] * N for _ in range(N)]
     cnt[0][0] = 0
     que = deque([(0,0)])
     
     while que:
         x, y = que.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
             if nx >= 0 and nx < N and ny >= 0 and ny < N and cnt[nx][ny] >= cnt[x][y] + 1:
                 que.append((nx, ny))
                 cnt[nx][ny] = min(cnt[nx][ny], cnt[x][y] + 1) if room[nx][ny] == 0 else min(cnt[x][y], cnt[nx][ny])
