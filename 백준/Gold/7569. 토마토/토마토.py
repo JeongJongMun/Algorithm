9657c3076ae5891a064914b90dfbc80baf84fc6f
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-24 18:04:03 +0900

    [Gold V] Title: 토마토, Time: 1912 ms, Memory: 57576 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,70 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+INF = 1e9
+# 위 아래 왼쪽 오른쪽 앞 뒤 - xyz
+dir = [(0, 0, 1), (0, 0, -1), (-1, 0, 0), (1, 0, 0), (0, 1, 0), (0, -1, 0)]
+cnt = goal = 0
+
+def bfs(tomato, day, m, n, h):
+    que = deque()
+
+    for k in range(h):
+        for j in range(n):
+            for i in range(m):
+                if tomato[k][j][i] == 1:
+                    que.append((i,j,k))
+    global cnt
+    
+    while que:
+        x, y, z = que.popleft()
+        for dx, dy, dz in dir:
+            nx, ny, nz = x + dx, y + dy, z + dz
+            if nx >= 0 and nx < len(tomato[0][0]) and ny >= 0 and ny < len(tomato[0]) and nz >= 0 and nz < len(tomato) and tomato[nz][ny][nx] == 0:
+                que.append((nx, ny, nz))
+                tomato[nz][ny][nx] = 1
+                day[nz][ny][nx] = day[z][y][x] + 1
+                cnt += 1
+
+def main():
+    m, n, h = map(int, input().split())
+    tomato = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+    day = [[[1] * m for _ in range(n)] for _ in range(h)]
+        
+    global cnt, goal
+    goal = m * n * h
+    for i in range(h):
+        for j in range(n):
+            for k in range(m):
+                if tomato[i][j][k] == 1:
+                    cnt += 1
+                elif tomato[i][j][k] == -1:
+                    goal -= 1
+
+ 
+    bfs(tomato, day, m, n, h)
+
+    # for mat in tomato:
+    #     print(*mat, sep='\n')
+    #     print()
+    # for mat in day:
+    #     print(*mat, sep='\n')
+    #     print()
+        
+    ans = 0
+    for mat in day:
+        ans = max(ans, (max(map(max, mat))))
+    print(ans - 1) if cnt == goal else print(-1)
+
+if __name__ == "__main__":
+    main()
+'''
+2 2 3
+-1 -1
+-1 -1
+1 -1
+0 0
+-1 0
+-1 0
+'''
