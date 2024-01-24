7cfb5afb5c8437d9d0941037db0f95fb660062a1
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-01-24 19:00:18 +0900

    [Gold V] Title: 토마토, Time: 1736 ms, Memory: 48424 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -7,7 +7,7 @@ INF = 1e9
 dir = [(0, 0, 1), (0, 0, -1), (-1, 0, 0), (1, 0, 0), (0, 1, 0), (0, -1, 0)]
 cnt = goal = 0
 
-def bfs(tomato, day, m, n, h):
+def bfs(tomato, m, n, h):
     que = deque()
 
     for k in range(h):
@@ -23,14 +23,12 @@ def bfs(tomato, day, m, n, h):
             nx, ny, nz = x + dx, y + dy, z + dz
             if nx >= 0 and nx < len(tomato[0][0]) and ny >= 0 and ny < len(tomato[0]) and nz >= 0 and nz < len(tomato) and tomato[nz][ny][nx] == 0:
                 que.append((nx, ny, nz))
-                tomato[nz][ny][nx] = 1
                 tomato[nz][ny][nx] = tomato[z][y][x] + 1
                 cnt += 1
 
 def main():
     m, n, h = map(int, input().split())
     tomato = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-    day = [[[1] * m for _ in range(n)] for _ in range(h)]
         
     global cnt, goal
     goal = m * n * h
@@ -43,11 +41,14 @@ def main():
                     goal -= 1
 
  
-    bfs(tomato, day, m, n, h)
+    bfs(tomato, m, n, h)
         
-    ans = 0
-    for mat in tomato:
-        ans = max(ans, (max(map(max, mat))))
+    # ans = 0
+    # for mat in tomato:
+    #     ans = max(ans, (max(map(max, mat))))
+        
+    ans = max(max(map(max, mat)) for mat in tomato)
+
     print(ans - 1) if cnt == goal else print(-1)
 
 if __name__ == "__main__":
