76c4356046e6258cb55c71c0404b59deb38bebad
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-01 10:48:39 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,16 +1,17 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
-if n == 1:
-    print(arr[0])
-    exit()
-elif n == 0:
-    print(0)
-    exit()
-    
-dp = [0] * n
-dp[0] = arr[0]
-dp[1] = arr[0] + arr[1]
-for i in range(2, len(dp)):
-    dp[i] = arr[i] + max(dp[i - 3] + arr[i - 1], dp[i - 2])
+import sys
+input = sys.stdin.readline
 
-print(dp[-1])
+def main():
+    n = int(input().strip())
+    arr = [int(input().strip()) for _ in range(n)]
+    if n == 1:
+        print(arr[0])
+        quit()
+    dp = [0] * n
+    dp[0] = arr[0]
+    dp[1] = arr[0] + arr[1]
+    for i in range(2, n):
+        dp[i] = max(arr[i - 1] + dp[i - 3], dp[i - 2]) + arr[i]
+    print(dp[-1])
+if __name__ == "__main__":
+    main()
