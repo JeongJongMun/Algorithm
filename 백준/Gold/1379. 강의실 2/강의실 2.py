290f598d7f3cab115ff740f1c63a5e10937d9bd3
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-01 12:00:58 +0900

    [Gold III] Title: 강의실 2, Time: 448 ms, Memory: 59384 KB -BaekjoonHub

--- 백준/Gold/1379. 강의실 2/강의실 2.py
+++ 백준/Gold/1379. 강의실 2/강의실 2.py
@@ -1,38 +1,37 @@
-import sys, heapq
-from collections import deque
+import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 def main():
     n = int(input().strip())
+    # lect 배열은 앞으로 넣어야 할 강의들 - 시작 시간 순으로 오름차순 정렬
     lect = []
     for _ in range(n):
         num, s, e = map(int, input().split())
         lect.append((s, e, num))
     lect.sort()
     
-    # print(*lect, sep='\n')
-    room, ans = [], [0] * (n + 1)
+    # room 배열은 배정된 강의실들 최소 힙 (종료 시간)
+    room = []
+    ans = [0] * (n + 1)
     s, e, num = lect[0]
-    ln = 1
     
-    heapq.heappush(room, (e, s, num, ln))
+    ln = 1
+    heappush(room, (e, s, num, ln))
     for l in lect[1:]:
         s, e, num = l
-        # print(*room)
         if s >= room[0][0]:
-            a = heapq.heappop(room)
-            heapq.heappush(room, (e, s, num, a[3]))
-            # print(a, '빼고', (e, s, num, a[3]), '넣음')
-            ans[a[2]] = a[3]
+            done = heappop(room)
+            heappush(room, (e, s, num, done[3]))
+            ans[done[2]] = done[3]
         else:
             ln += 1
-            heapq.heappush(room, (e, s, num, ln))
-            # print(e, s, num, ln, '넣음')
+            heappush(room, (e, s, num, ln))
     
     print(len(room))
 
     while room:
-        e, s, num, ln = heapq.heappop(room)
+        _, _, num, ln = heappop(room)
         ans[num] = ln
     
     print(*ans[1:], sep='\n')
