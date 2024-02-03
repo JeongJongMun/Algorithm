ca28efe6f2e91958bc8b3447b77fd94f3ed75bbf
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-03 23:12:10 +0900

    [Silver III] Title: 선분 위의 점, Time: 384 ms, Memory: 58012 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
@@ -3,13 +3,11 @@ from bisect import bisect_left, bisect_right
 input = sys.stdin.readline
 
 def main():
-    n, m = map(int, input().split())
+    _, m = map(int, input().split())
     dot = sorted(list(map(int, input().split())))
-    line = [list(map(int, input().split())) + [0] for _ in range(m)]
-    for idx, (s, e, c) in enumerate(line):
-        line[idx][2] = bisect_right(dot, e) - bisect_left(dot, s)
-    for s, e, c in line:
-        print(c)
+    line = [list(map(int, input().split())) for _ in range(m)]
+    for s, e in line:
+        print(bisect_right(dot, e) - bisect_left(dot, s))
     
 if __name__ == "__main__":
     main()
