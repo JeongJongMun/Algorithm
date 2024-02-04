1fc7966847f8ce9982b050596d86d29562804fbe
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-04 16:50:40 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -3,41 +3,38 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int N, M, answer; cin >> N >> M;
+	int N, M, answer = 0; cin >> N >> M;
 
 	vector<int> bluray(N);
 
-	int right = 0;
 	for (int i = 0; i < N; i++) {
 		cin >> bluray[i];
-		right += bluray[i];
 	}
 
 	int left = *max_element(bluray.begin(), bluray.end());
-	int mid;
+	int right = accumulate(bluray.begin(), bluray.end(), 0);
 	while (left <= right) {
-		mid = (left + right) / 2;
+		int mid = (left + right) / 2;
 		int bluray_sum = 0;
 		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-
-			if (bluray_sum + bluray[i] > mid) {
+		for (auto i : bluray) {
+			if (bluray_sum + i > mid) {
 				bluray_sum = 0;
 				cnt++;
 			}
-			bluray_sum += bluray[i];
+			bluray_sum += i;
 		}
+
 		if (bluray_sum != 0) cnt++;
 
-		if (cnt > M) {
-			left = mid + 1;
-		}
-		else if (cnt <= M) {
+		if (cnt > M) left = mid + 1;
+		else {
 			right = mid - 1;
 			answer = mid;
 		}
 	}
 
 	cout << answer;
+
 	return 0;
 }
