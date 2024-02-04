d4575b0318c7b4a4284ef379a10485be8cff28aa
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-04 16:25:42 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.cc
+++ 백준/Silver/2512. 예산/예산.cc
@@ -1,21 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, M; cin >> N;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N;
-	
 	vector<int> cost(N);
 
 	int max_cost = 0;
@@ -23,24 +12,21 @@ int main()
 		cin >> cost[i];
 		max_cost = max(max_cost, cost[i]);
 	}
-	
+
 	cin >> M;
 
-	int left = 1; // 0으로 하면 Division By Zero 런타임 에러 발생
+	int left = 1;
 	int right = max_cost;
 	int answer = 0;
+
+
 	while (left <= right) {
 		int mid = (left + right) / 2;
 		int sum = 0;
 
+		for (auto i : cost) sum += min(i, mid);
 
-		for (auto i : cost) {
-			sum += min(i, mid);
-		}
-
-		if (sum > M) {
-			right = mid - 1;
-		}
+		if (sum > M) right = mid - 1;
 
 		else {
 			answer = max(answer, mid);
@@ -49,6 +35,5 @@ int main()
 	}
 
 	cout << answer;
-
 	return 0;
 }
