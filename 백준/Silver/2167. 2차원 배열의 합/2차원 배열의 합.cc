241a16bb2d0da278fcbf32fe99a2d8c4bb26f2ce
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-04 17:19:48 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 12 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -1,16 +1,9 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, M, K;
-int arr[301][301];
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, M, K, arr[301][301];
 
 	cin >> N >> M;
 
@@ -21,7 +14,7 @@ int main()
 	}
 	for (int i = 1; i < N + 1; i++) {
 		for (int j = 1; j < M + 1; j++) {
-			arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1] [j - 1];
+			arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
 		}
 	}
 
