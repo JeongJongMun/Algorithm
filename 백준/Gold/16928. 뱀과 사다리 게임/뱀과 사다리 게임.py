95668912862e272cc20c98a4904e3dd83f5991d7
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-22 15:28:29 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -2,45 +2,36 @@ import sys
 input = sys.stdin.readline
 from heapq import heappop, heappush
 
-def bfs(ladder, snake):
-    graph = [0] * 101
-    visited = [False] * 101
+def bfs(jump):
+    graph = [-1] * 101
+    graph[1] = 0
+    # visited = [False] * 101
     que = [(0, 1)]
     
     while que:
         cost, node = heappop(que)
         for i in range(1, 7):
             n_node = node + i
-            if 0 <= n_node <= 100 and graph[n_node] <= graph[node] and not visited[n_node]:
-                if n_node in ladder:
-                    j_node = ladder[n_node]
-                elif n_node in snake:
-                    j_node = snake[n_node]
+            if n_node <= 100 and graph[n_node] <= graph[node] and graph[n_node] == -1:
+                if n_node in jump:
+                    heappush(que, (cost + 1, jump[n_node]))
+                    graph[jump[n_node]] = cost + 1
+                    # visited[jump[n_node]] = True
                 else:
-                    j_node = False
                     heappush(que, (cost + 1, n_node))
-                    
-                if j_node:
-                    heappush(que, (cost + 1, j_node))
-                    graph[j_node] = cost + 1
-                    visited[j_node] = True
                 graph[n_node] = cost + 1
-                visited[n_node] = True
+                # visited[n_node] = True
             
     return graph[100]
 
 def main():
     n, m = map(int, input().split())
-    ladder, snake = {}, {}
-    for i in range(n):
+    jump = {}
+    for _ in range(n + m):
         x, y = map(int, input().split())
-        ladder[x] = y
-    
-    for i in range(m):
-        u, v = map (int, input().split())
-        snake[u] = v
-    
-    print(bfs(ladder, snake))
+        jump[x] = y
+  
+    print(bfs(jump))
     
 if __name__ == "__main__":
     main()
