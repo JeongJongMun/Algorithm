eabb5dc233c473c0a3394e6e06e9ddfa06ec65c4
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-02-27 11:20:40 +0900

    [Gold V] Title: 두 용액, Time: 108 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input().strip())
+    water = list(map(int, input().split()))
+    water.sort()
+    
+    m = 1e10
+    low, high = 0, n - 1
+    alow, ahigh = 0, 0
+    
+    while low < high:
+        cur = abs(water[low] + water[high])
+        if (cur < m):
+            m = cur
+            alow, ahigh = water[low], water[high]
+        
+        if abs(water[low]) > abs(water[high]):
+            low += 1
+        else:
+            high -= 1
+
+    print(alow, ahigh)
+    
+if __name__ == "__main__":
+    main()
