1842ade20a538f3b21f84ea782e28c601b81ff0d
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-03-01 14:39:33 +0900

    [Gold I] Title: 대표 선수, Time: 1448 ms, Memory: 69592 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.py
+++ 백준/Gold/2461. 대표 선수/대표 선수.py
@@ -1,5 +1,6 @@
 import sys
 from heapq import heappop, heappush
+
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -7,12 +8,12 @@ INF = int(1e9)
 def main():
     n, m = map(int, input().split())
 
-    stu = [list(map(int, input().split())) for _ in range(n)]
-    idx = [1] * 1001
-    
-    for i in range(n):
-        stu[i].sort()
-    
+    stu = [sorted(list(map(int, input().split()))) for _ in range(n)]
+    idx = [1] * (n + 1)
+
+    # for i in range(n):
+    #     stu[i].sort()
+
     hq = []
     max_value = 0
     for i in range(n):
@@ -23,7 +24,7 @@ def main():
     while hq:
         min_value, index = heappop(hq)
         ans = min(ans, max_value - min_value)
-        
+
         if idx[index] == m:
             break
 
@@ -31,10 +32,9 @@ def main():
         heappush(hq, (next, index))
         max_value = max(max_value, next)
         idx[index] += 1
-        
 
-    
     print(ans)
+
 
 if __name__ == "__main__":
     main()
