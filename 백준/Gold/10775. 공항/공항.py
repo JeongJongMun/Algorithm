9a8a501e7bb0dd6b6bcb02169537577dbf6a6319
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-04-01 13:09:08 +0900

    [Gold II] Title: 공항, Time: 224 ms, Memory: 39612 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -2,21 +2,20 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    g = int(input().strip())
-    p = int(input().strip())
-    gate = [0 for _ in range(g + 1)]
+    g, p = int(input().strip()), int(input().strip())
+    gate = [False for _ in range(g + 1)]
+    *last_gate, = range(g + 1)
     airplains = [int(input().strip()) for _ in range(p)]
-    last_gate = [i for i in range(g + 1)]
     
     cnt = 0
     for i in range(p):
         flag = False
         for j in range(last_gate[airplains[i]], 0, -1):
             if not gate[j]:
-                gate[j] = 1
+                gate[j] = True
+                flag = True
                 last_gate[airplains[i]] = j
                 cnt += 1
-                flag = True
                 break
         if not flag:
             break
