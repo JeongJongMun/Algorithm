2008543975093e4c12acef87774a2d71759f4a4a
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-07-12 23:01:42 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,28 +1,25 @@
 import sys
-input = sys.stdin.readline
-cnt = 0
-            
-def dfs(graph, visited, start):
-    visited[start] = 1
-    global cnt
-    cnt += 1 if start != 1 else 0
-    for node in graph[start]:
-        if not visited[node]:
-            dfs(graph, visited, node)
-                
+sys.setrecursionlimit(10**6)
 
+def dfs(graph, visit, node, cnt = -1):
+    visit[node] = True
+    
+    for next in graph[node]:
+        if not visit[next]:
+            cnt = dfs(graph, visit, next, cnt)
+    
+    return cnt + 1
 def main():
-    v = int(input().strip())
-    e = int(input().strip())
+    v, e = int(input()), int(input())
     graph = [[] for _ in range(v + 1)]
-    visited = [0] * (v + 1)
+    visit = [False] * (v + 1)
     
     for _ in range(e):
-        a, b = map(int, input().split())
-        graph[a] += [b]
-        graph[b] += [a]
-        
-    dfs(graph, visited, 1)
-    print(cnt)
-if __name__ == '__main__':
+        start, end = map(int, input().split())
+        graph[start].append(end)
+        graph[end].append(start)
+    
+    answer = dfs(graph, visit, 1)
+    print(answer)
+if __name__ == "__main__":
     main()
