1c55d918501de7499693e403656998e633d65c2e
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-07-17 22:38:32 +0900

    [Gold IV] Title: 좋다, Time: 1200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
@@ -6,22 +9,21 @@ ans = 0
 for i in range(n):
     low, high = 0, n - 1
     
-    while low < high:
+    while low != high:
         if low == i:
             low += 1
+            continue
         elif high == i:
             high -= 1
+            continue
         
-        if low == high:
-            break
-
         sum = arr[low] + arr[high]
-        if sum == arr[i]:
-            ans += 1
-            break
         if sum < arr[i]:
             low += 1
-        else:
+        elif sum > arr[i]:
             high -= 1
+        else:
+            ans += 1
+            break
 
 print(ans)
