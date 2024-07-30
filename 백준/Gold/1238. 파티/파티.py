6c4f759dc08ae3ceeb89b6ffca01e693e9dfca95
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-07-31 02:55:04 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -5,8 +5,8 @@ INF = int(1e9)
 
 def dijkstra(_graph, start):
     n = len(_graph)
-    distances = [INF] * (n + 1)
-    distances[x] = 0
+    distances = [INF] * n
+    distances[start] = 0
     hq = [(0, start)]
 
     while hq:
@@ -17,24 +17,16 @@ def dijkstra(_graph, start):
                 distances[next_node] = next_cost
                 heappush(hq, (next_cost, next_node))
     
-    for i in range(n):
-        answer[i] += distances[i]
+    return distances
 
 n, m, x = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
+reversed_graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
+    reversed_graph[b].append((a, c))
 
-answer = [0] * (n + 1)
-
-dijkstra(graph, x)
-
-reversed_graph = [[] for _ in range(n + 1)]
-for i in range(1, n + 1):
-    for b, c in graph[i]:
-        reversed_graph[b].append((i, c))
-
-dijkstra(reversed_graph, x)
+answer = [a + b for a, b in zip(dijkstra(graph, x), dijkstra(reversed_graph, x))]
 
 print(max(answer[1:]))
