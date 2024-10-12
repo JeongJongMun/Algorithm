25b4cb2a1024cb9d8fd744d72f64adc9a503e1cd
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-10-12 23:37:21 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,114 +1,27 @@
-#include <vector>
-#include <stack>
 #include <iostream>
 using namespace std;
 
-enum State
-{
-	horizontal,
-	diagonal,
-	vertical,
-};
-
-struct Node
-{
-	int x;
-	int y;
-	enum State state;
-
-    Node(int x, int y, State state) : x(x), y(y), state(state) {}
-};
-
-
-int N;
-bool visit[16];
+int N, answer = 0;
 int graph[16][16];
-int answer = 0;
-stack<Node> st;
 
-void TryGoVertical(Node node)
+void dfs(int y, int x, int s) // 1 = hori, 2 = verti, 3 = diag
 {
-	int x = node.x;
-	int y = node.y;
-	State state = node.state;
+	if (y >= N or x >= N) return;
 
-	if (state == horizontal) return;
-
-	x++;
-	if (x < N && graph[x][y] != 1)
+	if (y == N - 1 && x == N - 1)
 	{
-		st.push(Node(x, y, vertical));
+		answer++;
+		return;
 	}
-}
 
-void TryGoDiagonal(Node node)
-{
-	int x = node.x;
-	int y = node.y;
-	State state = node.state;
+	if ((s == 1 or s == 3) and !graph[y][x + 1])
+		dfs(y, x + 1, 1);
 
-	x++;
-	y++;
-	if (x < N && y < N &&
-		graph[x][y] != 1 &&
-		graph[x - 1][y] != 1 &&
-		graph[x][y - 1] != 1)
-	{
-		st.push(Node(x, y, diagonal));
-	}
-}
+	if ((s == 2 or s == 3) and !graph[y + 1][x])
+		dfs(y + 1, x, 2);
 
-void TryGoHorizontal(Node node)
-{
-	int x = node.x;
-	int y = node.y;
-	State state = node.state;
-
-	y++;
-
-	if (state == vertical) return;
-
-	if (y < N && graph[x][y] != 1)
-	{
-		st.push(Node(x, y, horizontal));
-	}
-}
-
-void dfs(int x, int y, State state)
-{
-	st.push(Node(x, y, state));
-
-	while (!st.empty())
-	{
-        Node node = st.top();
-        st.pop();
-
-
-		if (node.x == N - 1 && node.y == N - 1)
-		{
-			answer++;
-			continue;
-		}
-
-		switch (node.state)
-		{
-			case horizontal:
-				TryGoHorizontal(node);
-				TryGoDiagonal(node);
-				break;
-
-			case vertical:
-				TryGoVertical(node);
-				TryGoDiagonal(node);
-				break;
-
-			case diagonal:
-				TryGoVertical(node);
-				TryGoHorizontal(node);
-				TryGoDiagonal(node);
-				break;
-		}
-	}
+	if (!graph[y + 1][x + 1] and !graph[y + 1][x] and !graph[y][x + 1])
+		dfs(y + 1, x + 1, 3);
 }
 
 int main() {
@@ -117,14 +30,10 @@ int main() {
 	cin >> N;
 	int x;
 	for (int i = 0; i < N; i++)
-	{
 		for (int j = 0; j < N; j++)
-		{
 			cin >> graph[i][j];
-		}
-	}
 
-	dfs(0, 1, horizontal);
+	dfs(0, 1, 1);
 
 	cout << answer;
 
