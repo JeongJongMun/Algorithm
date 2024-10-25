a73516ab27e19f5919b7acb8b57767e248889cce
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-10-25 22:16:48 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
@@ -10,28 +10,24 @@ int main()
 	
 	int N; cin >> N;
 	int s, e;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> meetings;
+	vector<pair<int, int>> m;
 	priority_queue<int, vector<int>, greater<>> pq;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> s >> e;
-		meetings.emplace(s, e);
+		m.emplace_back(s, e);
 	}
-	
-	auto [a, b] = meetings.top();
-	meetings.pop();
-	pq.emplace(b);
-	int ans = 1;
-	
-	while (!meetings.empty())
-	{
-		auto [start, end] = meetings.top();
-		meetings.pop();
 
-		while (!pq.empty() and pq.top() <= start)
+	sort(m.begin(), m.end());
+	pq.emplace(m[0].second);
+	
+	int ans = 1;
+	for (int i = 1; i < N; i++)
+	{
+		while (!pq.empty() and pq.top() <= m[i].first)
 			pq.pop();
 
-		pq.emplace(end);
+		pq.emplace(m[i].second);
 		ans = max(ans, (int)pq.size());
 	}
 	
