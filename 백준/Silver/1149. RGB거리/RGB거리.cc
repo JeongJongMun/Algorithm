b703119ac378f8c8860851db9765a4525ec83db2
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-11-09 14:56:26 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,29 +1,37 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 using namespace std;
 
-int n;
-int arr[3][1001];
-int dp[3][1001];
+int rgb[1000][3];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[0][i] >> arr[1][i] >> arr[2][i];
-    }
+	int N, color; cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> color;
+			rgb[i][j] = color;
+		}
+	}
 
-    for (int i = 0; i < n; i++) {
-        dp[0][i] = arr[0][i] + min({ dp[0][i - 2] + min(arr[1][i - 1], arr[2][i - 1]), dp[1][i - 1], dp[2][i - 1]});
-        dp[1][i] = arr[1][i] + min({ dp[1][i - 2] + min(arr[0][i - 1], arr[2][i - 1]), dp[0][i - 1], dp[2][i - 1]});
-        dp[2][i] = arr[2][i] + min({ dp[2][i - 2] + min(arr[0][i - 1], arr[1][i - 1]), dp[0][i - 1], dp[1][i - 1]});
-    }
-
-    cout << min({ dp[0][n - 1], dp[1][n - 1], dp[2][n - 1] }) << endl;
-
-    return 0;
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (j == 0)
+				rgb[i][j] += min(rgb[i - 1][1], rgb[i - 1][2]);
+			else if (j == 1)
+				rgb[i][j] += min(rgb[i - 1][0], rgb[i - 1][2]);
+			else if (j == 2)
+				rgb[i][j] += min(rgb[i - 1][0], rgb[i - 1][1]);
+		}
+	}
+	
+	cout << *min_element(rgb[N - 1], rgb[N - 1] + 3);
+	
+	return 0;
 }
