13b8362c185503a5c7e27084aa87aa0ab3dee398
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2024-12-21 23:31:27 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,32 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
+
 using namespace std;
+int arr[1000001];
+int L[1000001];
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int main()
-{
-    int N; cin >> N;
-    vector<int> arr(N);
-    vector<int> dp;
-    for (int i = 0; i < N; i++)
-        cin >> arr[i];
-
-    dp.push_back(arr[0]);
-    
-    for (int i = 1; i < N; i++)
-    {
-        if (dp.back() < arr[i])
-            dp.push_back(arr[i]);
-        else
-        {
-            auto it = lower_bound(dp.begin(), dp.end(), arr[i]);
-            auto idx = distance(dp.begin(), it);
-            dp[idx] = arr[i];
+    int n; cin >> n;
+    int temp;
+    int idx = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> temp;
+        if (idx == 0) L[idx++] = temp;
+        else {
+            if (L[idx - 1] < temp) L[idx++] = temp;
+            else {
+                L[lower_bound(L, L + idx, temp) - L] = temp;
+            }
         }
-
     }
-
-    cout << dp.size() << '\n';
-    
-    return 0;
+    cout << idx;
 }
