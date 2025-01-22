af76f873b1811efb0efb18670d7c1bfe16f839d9
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2025-01-22 15:17:24 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -8,24 +8,17 @@ int main()
     int N; cin >> N;
     int l, r;
     vector<pair<int, int>> arr(N);
-    for (int i = 0; i < N; i++)
-    {
+    for (auto &[l, r] : arr)
         cin >> l >> r;
-        arr[i] = make_pair(l, r);
-    }
-
+    
     sort(arr.begin(), arr.end());
-
-    vector<int> rights;
-    for (auto [left, right] : arr)
-        rights.push_back(right);
 
     vector dp(N, 1);
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < i; j++)
         {
-            if (rights[i] > rights[j])
+            if (arr[i].second > arr[j].second)
                 dp[i] = max(dp[i], dp[j] + 1);
         }
     }
