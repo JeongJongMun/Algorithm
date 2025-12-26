1559f0420fbf46cea57ebc4f6534b60b629f4cd0
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2025-12-26 17:02:23 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,25 +1,38 @@
-#include<iostream>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-using namespace std;
-int arr[1000001];
-int L[1000001];
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
-    int n; cin >> n;
-    int temp;
-    int idx = 0;
-    for (int i = 1; i <= n; i++) {
-        cin >> temp;
-        if (idx == 0) L[idx++] = temp;
-        else {
-            if (L[idx - 1] < temp) L[idx++] = temp;
-            else {
-                L[lower_bound(L, L + idx, temp) - L] = temp;
-            }
+    int n;
+    std::cin >> n;
+    
+    std::vector<int> arr(n);
+    std::vector<int> dp;
+    
+    for (auto& elem : arr)
+    {
+        std::cin >> elem;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        auto iter = std::lower_bound(dp.begin(), dp.end(), arr[i]);
+        if (iter == dp.end())
+        {
+            dp.push_back(arr[i]);
+        }
+        else
+        {
+            *iter = arr[i];
         }
     }
-    cout << idx;
+    
+    std::cout << dp.size() << "\n";
+    
+    return 0;
 }
