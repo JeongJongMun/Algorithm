c2e7f6d0a7318972e9efaa4b3090f0da8a64649b
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2026-05-17 23:35:50 +0900

    [level 1] Title: 노란불 신호등, Time: 1.73 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -16,30 +15,19 @@ int lcm(int a, int b)
 
 int solution(vector<vector<int>> signals) 
 {
-    int answer = 0;
-    int signal_count = signals.size();
-    
     // 신호등 주기(green + yellow + red)들이 담길 배열
     vector<int> signal_periods;
     
-    for (const auto& signal : signals)
-    {
-        // 신호등 주기 합
-        int signal_period = 0;
-        for (const auto& color : signal)
-        {
-            signal_period += color;
-        }
-        signal_periods.push_back(signal_period);
-    }
-    
     // 반복해야 하는 횟수 = 신호등 주기들의 최소공배수
     int signal_lcm = 1;
-    for (const auto& signal_period : signal_periods)
+    
+    for (const auto& signal : signals)
     {
+        int signal_period = signal[0] + signal[1] + signal[2];
+        signal_periods.push_back(signal_period);
         signal_lcm = lcm(signal_period, signal_lcm);
     }
-    
+
     // T = green + yellow + red
     // t초일 때, 주기 내 상대적인 위치는 (t-1) % T
     // if 0 <= x < green -> 초록불
@@ -47,22 +35,21 @@ int solution(vector<vector<int>> signals)
     // if green + yellow <= x < green + yello + red -> 빨간불
     for (int t = 1; t <= signal_lcm; t++)
     {
-        bool isThisTimeAllYellow = true;
+        bool is_all_yellow = true;
         
-        for (int i = 0; i < signal_count; i++)
+        for (int i = 0; i < signals.size(); i++)
         {
             int T = signal_periods[i];
             int pos = (t - 1) % T;
             
-            bool isYellow = signals[i][0] <= pos && pos < signals[i][0] + signals[i][1];
-            if (!isYellow)
+            if (!(signals[i][0] <= pos && pos < signals[i][0] + signals[i][1]))
             {
-                isThisTimeAllYellow = false;
+                is_all_yellow = false;
                 break;
             }
         }
         
-        if (isThisTimeAllYellow)
+        if (is_all_yellow)
         {
             return t;
         }
