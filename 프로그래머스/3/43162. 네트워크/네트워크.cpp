4ae1eeb85d80378543ea261b8050519c2a6d3e3d
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2026-06-03 16:12:09 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stack>
 
-bool dfs(std::vector<std::vector<int>>& graph, std::vector<bool>& visited, int start)
+void dfs(std::vector<std::vector<int>>& graph, std::vector<bool>& visited, int start)
 {
-    if (visited[start])
+    for (int i = 0; i < graph.size(); i++)
     {
-        return false;
-    }
-    
-    int n  = graph.size();
-
-    std::stack<int> s;
-    s.push(start);
-    
-    while (!s.empty())
-    {
-        int node = s.top();
-        s.pop();
-        
-        for (int i = 0; i < n; i++)
+        if (start == i || graph[start][i] == 0 || visited[i])
         {
-            if (node == i || !graph[node][i] || visited[i])
-            {
-                continue;
-            }
-            s.push(i);
-            visited[i] = true;
+            continue;
         }
+        visited[i] = true;
+        dfs(graph, visited, i);
     }
-    
-    return true;
 }
 
 int solution(int n, std::vector<std::vector<int>> graph) 
 {
     std::vector<bool> visited(n, false);
-    
     int answer = 0;
+    
     for (int i = 0; i < n; i++)
     {
-        if (dfs(graph, visited, i))
+        if (visited[i])
         {
-            answer++;
+            continue;
         }
+        
+        dfs(graph, visited, i);
+        answer++;
     }
     
     return answer;
