037e0ba6e09d4414de261205f4b69a797b2701e0
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2026-06-14 13:57:03 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 2.65 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/README.md
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 퍼즐 조각 채우기 - 84021 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84021) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84021#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 14일 13:56:04
+2026년 06월 14일 13:57:01
 
 ### 문제 설명
 
--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
@@ -9,149 +9,103 @@ class BlockData
 {
 public:
     
+    BlockData(int n)
+    {
+        shape.resize(n, std::vector<bool>(n));
+    }
+    
+    
     bool operator==(const BlockData& other)
     {
-        if (points.size() != other.points.size())
+        if (shape.size() != other.shape.size())
         {
             return false;
         }
-        
-        for (int i = 0; i < points.size(); i++)
+
+        for (int i = 0; i < shape.size(); i++)
         {
-            if (points[i].first != other.points[i].first ||
-                points[i].second != other.points[i].second)
+            if (shape[i] != other.shape[i])
             {
                 return false;
             }
         }
+        
         return true;
     }
     
-    void rotate()
-    {
-        for (auto& point : points)
-        {
-            // (y, x) -> (x, -y)
-            int y = point.first;
-            int x = point.second;
-            point.first = x;
-            point.second = -y;
-        }
-        
-        normalize();
-    }
-    
-    void normalize()
-    {
-        int min_x = 10, min_y = 10;
-        for (const auto& point : points)
-        {
-            min_x = std::min(min_x, point.second);
-            min_y = std::min(min_y, point.first);
-        }
-        
-        for (auto& point : points)
-        {
-            point.first -= min_y;
-            point.second -= min_x;
-        }
-        
-        std::sort(points.begin(), points.end());
-    }
-    
-    std::vector<std::pair<int, int>> points;
+    std::vector<std::vector<bool>> shape;
+
 };
 
 void dfs(const std::vector<std::vector<int>>& graph,
          std::vector<std::vector<bool>>& visited,
          std::vector<std::pair<int, int>>& block_pixels,
-         int cy, int cx, bool is_table)
+         int cx, int cy)
 {
     int n = graph.size();
     visited[cy][cx] = true;
-    block_pixels.push_back({cy, cx});
+    block_pixels.push_back({cx, cy});
     
     for (int i = 0; i < 4; i++)
     {
         int nx = cx + dx[i];
         int ny = cy + dy[i];
         
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[ny][nx] && graph[ny][nx] == (is_table ? 1 : 0))
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[ny][nx] && graph[ny][nx] == 1)
         {
-            dfs(graph, visited, block_pixels, ny, nx, is_table);        
+            dfs(graph, visited, block_pixels, nx, ny);            
         }
     }
 }
 
-void MakeBlockData(std::vector<std::vector<int>>& graph, std::vector<BlockData>& data, bool is_table)
+std::vector<BlockData> MakeBlockData(std::vector<std::vector<int>>& graph)
 {
     int N = graph.size();
     std::vector<std::vector<bool>> visited(N, std::vector<bool>(N, false));
+    std::vector<BlockData> data;
     
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            if (graph[i][j] != (is_table ? 1 : 0) || visited[i][j]) continue;
+            if (graph[i][j] != 1 || visited[i][j]) continue;
             
             std::vector<std::pair<int, int>> block_pixels;
-            dfs(graph, visited, block_pixels, i, j, is_table);            
+            dfs(graph, visited, block_pixels, j, i);            
             
-            int min_x = N, min_y = N;
+            int min_x, min_y = N;
+            int max_x, max_y = -1;
             for (const auto& pixel : block_pixels)
             {
-                min_y = std::min(min_y, pixel.first);
-                min_x = std::min(min_x, pixel.second);
+                min_x = std::min(min_x, pixel.first);
+                min_y = std::min(min_y, pixel.second);
+                max_x = std::max(max_x, pixel.first);
+                max_y = std::max(max_y, pixel.second);
             }
             
-            BlockData block;
+            int length_x = max_x - min_x + 1;
+            int length_y = max_y - min_y + 1;
+            int length = std::max(length_x, length_y);
+            BlockData block(length);
             
             for (const auto& pixel : block_pixels)
             {
-                int ry = pixel.first - min_y;
-                int rx = pixel.second - min_x;
-                block.points.push_back({ry, rx}); 
+                int rx = pixel.first - min_x;
+                int ry = pixel.second - min_y;
+                block.shape[ry][rx] = true; 
             }
-            std::sort(block.points.begin(), block.points.end());
             data.push_back(block);
         }
-    }    
+    }
+    
+    return data;
 }
 
 int solution(std::vector<std::vector<int>> game_board, std::vector<std::vector<int>> table)
 {
-    std::vector<BlockData> table_data;
-    std::vector<BlockData> board_data;
-    MakeBlockData(table, table_data, true);
-    MakeBlockData(game_board, board_data, false);
+    std::vector<BlockData> table_data = MakeBlockData(table);
+    std::vector<BlockData> board_data = MakeBlockData(game_board);
     
-    std::vector<bool> used(table_data.size(), false);
-    
-    int answer = 0;
-
-    for (int i = 0; i < board_data.size(); i++)
-    {
-        bool is_match = false;
-        for (int j = 0; j < table_data.size(); j++)
-        { 
-            if (used[j]) continue;
-            
-            BlockData original_block = table_data[j];
-            
-            for (int k = 0; k < 4; k++)
-            {
-                if (original_block == board_data[i])
-                {
-                    answer += original_block.points.size();
-                    used[j] = true;
-                    is_match = true;
-                    break;
-                }
-                original_block.rotate();
-            }
-            if (is_match) break;
-        }
-    }
-    
+    int answer = -1;
     return answer;
 }
