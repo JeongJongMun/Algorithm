8fc2742bf19d842ab3876ebc8431a0ba193bc4ca
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2026-06-14 15:20:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 7.26 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,14 +1,11 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <queue>
-#include <unordered_map>
+#include <set>
+#include <iterator>
 
 std::vector<int> solution(std::vector<std::string> operations) 
 {
-    std::priority_queue<int, std::vector<int>, std::greater<int>> min_pq;
-    std::priority_queue<int> max_pq;
-    std::unordered_map<int, int> count;
+    std::multiset<int> ms;
     
     for (const auto& op : operations)
     {
@@ -16,56 +13,27 @@ std::vector<int> solution(std::vector<std::string> operations)
         int num = std::stoi(op.substr(2, op.size()));
         if (command == 'I')
         {
-            min_pq.push(num);
-            max_pq.push(num);
-            count[num]++;
+            ms.insert(num);
         }
         else if (num == 1)
         {
-            while (!max_pq.empty() && count[max_pq.top()] == 0)
+            if (!ms.empty())
             {
-                max_pq.pop();
-            }
-            if (!max_pq.empty())
-            {
-                int num = max_pq.top();
-                max_pq.pop();
-                count[num]--;                
+                ms.erase(std::prev(ms.end()));
             }
         }
         else if (num == -1)
         {
-            while (!min_pq.empty() && count[min_pq.top()] == 0)
+            if (!ms.empty())
             {
-                min_pq.pop();
+                ms.erase(ms.begin());
             }
-            if (!min_pq.empty())
-            {
-                int num = min_pq.top();
-                min_pq.pop();
-                count[num]--;                
-            }
+
         }
     }    
 
-    while (!max_pq.empty() && count[max_pq.top()] == 0)
-    {
-        max_pq.pop();
-    }
-    while (!min_pq.empty() && count[min_pq.top()] == 0)
-    {
-        min_pq.pop();
-    }
     std::vector<int> answer;
-    
-    // 653 -45
-    // 654 -45
-    
-    int max_elem = max_pq.empty() ? 0 : max_pq.top();
-    int min_elem = min_pq.empty() ? 0 : min_pq.top();
-    
-    answer.push_back(max_elem);
-    answer.push_back(min_elem);
-    
+    answer.push_back(ms.empty() ? 0 : *ms.rbegin());
+    answer.push_back(ms.empty() ? 0 : *ms.begin());
     return answer;
 }
