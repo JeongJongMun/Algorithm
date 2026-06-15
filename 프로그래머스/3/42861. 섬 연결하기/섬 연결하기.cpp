a4b4bac6443904bc3ee505197c58d6770a8f0fd3
정종문 <101979073+JeongJongMun@users.noreply.github.com>
2026-06-15 16:26:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+
+struct Edge
+{
+    int s;
+    int e;
+    int c;
+    
+    Edge(int start, int end, int cost) : s(start), e(end), c(cost) { }
+    
+    bool operator<(const Edge& other) const
+    {
+        return c > other.c;
+    }
+};
+
+int solution(int n, std::vector<std::vector<int>> costs) 
+{    
+    std::priority_queue<Edge> pq;
+    std::vector<std::vector<Edge>> graph(n, std::vector<Edge>());
+    for (const auto& cost : costs)
+    {
+        graph[cost[0]].emplace_back(cost[0], cost[1], cost[2]);
+        graph[cost[1]].emplace_back(cost[1], cost[0], cost[2]);
+    }
+    std::vector<bool> visited(n, false);
+    visited[0] = true;
+    int answer = 0;
+
+    for (const auto& edge : graph[0])
+    {
+        pq.emplace(edge);
+    }
+
+    while (!pq.empty())
+    {
+        Edge edge = pq.top();
+        pq.pop();
+        
+        if (visited[edge.e]) continue;
+
+        answer += edge.c;
+        visited[edge.e] = true;
+        
+        for (const auto& next : graph[edge.e])
+        {
+            if (!visited[next.e])
+            {
+                pq.emplace(next);
+            }
+        }
+    }
+    
+    
+    
+    return answer;
+}
